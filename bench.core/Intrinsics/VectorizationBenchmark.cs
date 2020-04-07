3666cd887a6dd11c20f6fc9ad7c15b81e479fd69
Maksym Badii <maksym.badii@pwc.com>
2020-04-07 12:16:11 -0500

    Test for +/* of vectors. Should for slower w/ old JIT. Parse args

---
+++ bench.core/Intrinsics/VectorizationBenchmark.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace bench.core.Intrinsics
+{
+    //https://github.com/EgorBo/IntrinsicsPlayground
+    [RankColumn]
+    [LegacyJitX86Job]
+    [RyuJitX64Job]
+    public class VectorizationBenchmark
+    {
+        private Vector4 vectorA, vectorB, vectorC = default;
+        private MyCustomVector4 myVectorA, myVectorB, myVectorC = default;
+
+        [Benchmark]
+        public void CustomMul() => myVectorC = myVectorA * myVectorB;
+        [Benchmark]
+        public void SystemMul() => vectorC = vectorA * vectorB;
+
+        [Benchmark]
+        public void CustomAdd() => myVectorC = myVectorA + myVectorB;
+        [Benchmark]
+        public void SystemAdd() => vectorC = vectorA + vectorB;
+
+    }
+
+    public struct MyCustomVector4
+    {
+        public float X, Y, Z, W;
+        public MyCustomVector4(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static MyCustomVector4 operator *(MyCustomVector4 left, MyCustomVector4 right) => new MyCustomVector4(
+            left.X * right.X,
+            left.Y * right.Y,
+            left.Z * right.Z,
+            left.W * right.W);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static MyCustomVector4 operator +(MyCustomVector4 left, MyCustomVector4 right) => new MyCustomVector4(
+           left.X + right.X,
+           left.Y + right.Y,
+           left.Z + right.Z,
+           left.W + right.W);
+    }
+
+}
--- bench.core/Program.cs
+++ bench.core/Program.cs
@@ -1,29 +1,23 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
+using System;
 using System.Linq;
 using System.Reflection;
-using bench.core.Intrinsics;
-using bench.core.Other;
-using BenchmarkDotNet.Running;
 
 namespace bench.core
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => BenchmarkRunner.Run(ParseArgs(args));
+
+        private static Type ParseArgs(string[] args)
         {
             if (!args.Any())
             {
                 Console.WriteLine($"\nSpecify valid test name");
-                return;
+                return null;
             }
             var availableBenchmarks = Assembly.GetExecutingAssembly().GetExportedTypes().Where(x => x.Name.EndsWith("Benchmark"));
-            var benchmarkToRun = availableBenchmarks.FirstOrDefault(x => x.Name == args.First());
-            if (benchmarkToRun == null)
-            {
-                Console.WriteLine($"\nSpecify valid test name");
-                return;
-            }
-            BenchmarkRunner.Run(benchmarkToRun);
+            return availableBenchmarks.FirstOrDefault(x => x.Name == args[0]);
         }
     }
 }
