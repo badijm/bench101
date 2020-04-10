1d9213b39c2a01ceb0dbab546f5f06d236cc4fd5
Maksym Badii <maksym.badii@pwc.com>
2020-04-10 03:25:10 -0500

    LOH - just to show this number: 85000b

---
+++ bench.core/GC/LOHBenchmark.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace bench.core.GC
+{
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class LOHBenchmark
+    {
+        [Benchmark]
+        public byte[] Allocate84900()
+        {
+            return new byte[84900];
+        }
+        [Benchmark]
+        public byte[] Allocate85000()
+        {
+            return new byte[85000];
+        }
+    }
+}
